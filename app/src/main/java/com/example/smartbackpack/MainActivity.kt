4997ffeebf0154a526e0d68d9667d9eb17d5fe2d
Daniil Surmachenko <vxlkxvik@yandex.ru>
2022-02-05 02:07:08 +0300

    Set up general navigation with bottom nav

--- app/src/main/java/com/example/smartbackpack/MainActivity.kt
+++ app/src/main/java/com/example/smartbackpack/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.smartbackpack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val bottomNav: BottomNavigationView = findViewById(R.id.bottom_nav)
+
+        // Set up bottom navigation
+        bottomNav.let {
+            NavigationUI.setupWithNavController(it, navController)
+        }
     }
 }
